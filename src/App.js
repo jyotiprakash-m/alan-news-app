dbdf636e91fccc9b7e7c327cc4c33c06a3eb2034
Jyotiprakash Mohanta <jpmohanta32@gmail.com>
2021-01-17 12:10:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
+import wordsToNumbers from 'words-to-numbers';
+import useStyles from './styles.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey= 'a7df4557b11936cc7f72b5673a4299522e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () =>{
+    const [newsArticles,setNewsArticles]= useState([]);
+    const [activeArticle,setActiveArticle] = useState(-1);
+    const classes =useStyles();
+
+    useEffect (() => {
+        alanBtn({
+            key:alanKey,
+            onCommand: ({command,articles,number}) =>{
+                if(command === 'newHeadlines'){
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if(command==='highlight'){
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle+1);
+                }else if(command==='open'){
+                    const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+
+                    if (parsedNumber > 20) {
+                        alanBtn().playText('Please try that again...');
+                    } else if (article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...');
+                    } else {
+                        alanBtn().playText('Please try that again...');
+                    }
+                   
+                }
+            },
+        });
+    },[])
+    return(
+        <div>
+            <div className={classes.logoContainer}>
+                <img className={classes.alanLogo} src="https://storage.pixteller.com/designs/designs-images/2020-09-09/09/logo-design-branding-logo-shapes-write-circular-1-5f591bcc67b8e.png" alt=""/>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+        </div>
+    )
 }
 
 export default App;
